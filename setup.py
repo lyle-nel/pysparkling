706bfcb6faf03f35a3a395b291505738395fa456
Lyle Nel <lyle-nel@users.noreply.github.com>
2019-11-04 07:24:28 +0200

    Figuring out pip error

--- setup.py
+++ setup.py
@@ -35,8 +35,7 @@ setup(
     install_requires=[
         'boto>=2.36.0',
         'future>=0.15',
-        'requests>=2.6.0',
-        'pytz-2019.3'
+        'requests>=2.6.0'
     ],
     extras_require={
         'hdfs': ['hdfs>=2.0.0'],
